e19f76e31831d546af55db2f2eb01363c36a9c8e
kaggullagiri <girikola111@gmail.com>
2023-11-14 17:28:26 +0530

    committed

--- Assignment 2/Assignment-2.py
+++ Assignment 2/Assignment-2.py
@@ -2,13 +2,10 @@
 # number in a list
 
 # list of numbers
-list1 = [10, 20, 104, 45, 99]
+list1 = [10, 20, 4, 45, 99]
 
 # sorting the list
 list1.sort()
 
 # printing the last element
 print("Largest element is:", list1[-1])
-
-#code is completed
-
